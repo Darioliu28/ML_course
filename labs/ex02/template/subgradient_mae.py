d9f4ed14fcb03a0ff8f1aead428d6b74d30f8bca
Darioliu28 <dario.liuzzo@gmail.com>
2025-09-17 14:51:47 +0200

    Added implementation for subgradient methods

--- labs/ex02/template/subgradient_mae.py
+++ labs/ex02/template/subgradient_mae.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from costs import *
+from helpers import batch_iter
 
 def compute_subgradient_mae(y, tx, w):
     """Compute a subgradient of the MAE at w.
@@ -12,8 +13,74 @@ def compute_subgradient_mae(y, tx, w):
     Returns:
         An array of shape (2, ) (same shape as w), containing the subgradient of the MAE at w.
     """
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: compute subgradient gradient vector for MAE
-    # ***************************************************
-    raise NotImplementedError
+    N=y.shape[0]
+    e=y-tx@w
+    g=-(1/N)*tx.T@np.sign(e)
+    return g
+
+def subgradient_descent(y, tx, initial_w, max_iters, gamma):
+    """The SubGradient Descent (SubGD) algorithm.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
+        max_iters: a scalar denoting the total number of iterations of GD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        losses: a list of length max_iters containing the loss value (scalar) for each iteration of SubGD
+        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of SubGD
+    """
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        loss=compute_loss(y,tx,w,method="MAE")
+        w=w-gamma*compute_subgradient_mae(y,tx,w)
+
+        ws.append(w)
+        losses.append(loss)
+        print(
+            "SubGD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
+                bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
+            )
+        )
+
+    return losses, ws
+
+def stochastic_subgradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
+    """Compute a stochastic subgradient at w from a data sample batch of size B, where B < N, and their corresponding labels.
+
+    Args:
+        y: numpy array of shape=(B, )
+        tx: numpy array of shape=(B,2)
+        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
+        batch_size: a scalar denoting the number of data points in a mini-batch used for computing the stochastic subgradient
+        max_iters: a scalar denoting the total number of iterations of SubSGD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        losses: a list of length max_iters containing the loss value (scalar) for each iteration of SubSGD
+        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of SubSGD
+    """
+
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+
+    for n_iter in range(max_iters):
+        y_batch, tx_batch = next(batch_iter(y, tx, batch_size))
+        loss=compute_loss(y_batch,tx_batch,w,method="MAE")
+        w=w-gamma*compute_subgradient_mae(y_batch,tx_batch,w)
+
+        ws.append(w)
+        losses.append(loss)
+        print(
+            "SubSGD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
+                bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
+            )
+        )
+    return losses, ws
